c0430fcefadaf2f715ed25cefbd2abb2d3084a39
kunjian chen <kunjianchen@kunjiandemacbook-pro.local>
2020-09-14 18:16:22 +0800

    Initial Commit

---
+++ opencvTest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencvTest
+//
+//  Created by kunjian chen on 2020/9/14.
+//  Copyright © 2020年 kunjian chen. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
